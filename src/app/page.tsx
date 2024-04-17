0d399bc9a1529462c5105ffdc9ab11211ac0fbea
Christian Stough <cstough97@gmail.com>
2024-04-16 22:45:27 -0500

    added force dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import {
   InputOTPSlot,
 } from "~/components/ui/input-otp";
 
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
   const data: LyricsData = await getLyrics();
 
