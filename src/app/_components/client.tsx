3e094c9a1950e187fcd29ffdf45539535c36a15a
Christian Stough <cstough97@gmail.com>
2024-04-17 17:47:52 -0500

    added traking of value

--- src/app/_components/client.tsx
+++ src/app/_components/client.tsx
@@ -14,17 +14,40 @@ export default function Client({
   randomLine: string;
   lineAfterRandom: string;
 }) {
-  const [valueArray, setValueArray] = useState<string[]>([]);
-
   const lineArray: string[] = randomLine.split(" ");
+
+  const [valueArray, setValueArray] = useState<string[]>(
+    new Array(lineArray.length).fill(""),
+  );
 
   return (
     <div className="flex h-full w-full flex-col items-center justify-center gap-8 text-4xl font-semibold">
       <div className="">{lineBeforeRandom}</div>
-      <MissingWord lineArray={lineArray} />
+      <div className="flex w-full max-w-2xl flex-wrap justify-center gap-8 px-4">
+        {lineArray.map((word, index) => (
+          <MissingWord
+            key={`${word}-${index}`}
+            word={word}
+            handleChange={(value: string, letterIndex: number) => {
+              const newValueArray = [...valueArray];
+              const splitValue = newValueArray[index]?.split("");
+
+              if (!splitValue) return;
+
+              splitValue[letterIndex] = value;
+
+              newValueArray[index] = splitValue.join("");
+              setValueArray(newValueArray);
+            }}
+          />
+        ))}
+      </div>
       <div>{lineAfterRandom}</div>
       <Button className="w-[250px]">Submit</Button>
       <div className=" absolute top-2 rounded-sm border bg-zinc-600 p-4">
+        {valueArray.join(" ")}
+      </div>
+      <div className=" absolute bottom-2 rounded-sm border bg-zinc-600 p-4">
         {randomLine}
       </div>
     </div>
--- src/app/_components/missing_word.tsx
+++ src/app/_components/missing_word.tsx
@@ -1,63 +1,78 @@
 import { Input } from "~/components/ui/input";
 
-export default function MissingWord({ lineArray }: { lineArray: string[] }) {
-  return (
-    <div className="flex w-full max-w-2xl flex-wrap justify-center gap-8 px-4">
-      {lineArray.map((word) => {
-        const splitWord = word.split("");
+export default function MissingWord({
+  word,
+  handleChange,
+}: {
+  word: string;
+  handleChange: (value: string, letterIndex: number) => void;
+}) {
+  const splitWord = word.split("");
 
+  return (
+    <div className="flex gap-2" key={word}>
+      {splitWord.map((letter, index) => {
+        if (letter === "'")
+          return (
+            <div className="-translate-y-4" key={`${word}-${letter}`}>
+              &apos;
+            </div>
+          );
         return (
-          <div className="flex gap-2" key={word}>
-            {splitWord.map((letter) => {
-              if (letter === "'")
-                return (
-                  <div className="-translate-y-4" key={`${word}-${letter}`}>
-                    &apos;
-                  </div>
-                );
-              return (
-                <Input
-                  className="w-8 overflow-visible px-0 text-center"
-                  key={`${word}-${letter}`}
-                  maxLength={1}
-                  onChange={(e) => {
-                    if (e.target.value.length === e.target.maxLength) {
-                      let nextInput = e.target
-                        .nextElementSibling as HTMLInputElement;
-                      // If the next element is not an input, move to the next one
+          <Input
+            className="w-8 overflow-visible px-0 text-center"
+            key={`${word}-${letter}`}
+            maxLength={1}
+            onChange={(e) => {
+              handleChange(e.target.value, index);
+
+              if (e.target.value.length === e.target.maxLength) {
+                let nextInput = e.target.nextElementSibling as HTMLInputElement;
+                // If the next element is not an input, move to the next one
+                while (nextInput && nextInput.tagName !== "INPUT") {
+                  nextInput = nextInput.nextElementSibling as HTMLInputElement;
+                }
+                if (!nextInput) {
+                  const parent = e.target.parentElement;
+                  if (parent) {
+                    const nextParent = parent.nextElementSibling as HTMLElement;
+                    if (nextParent) {
+                      nextInput =
+                        nextParent.firstElementChild as HTMLInputElement;
+                      // If the first child is not an input, move to the next one
                       while (nextInput && nextInput.tagName !== "INPUT") {
                         nextInput =
                           nextInput.nextElementSibling as HTMLInputElement;
                       }
-                      if (!nextInput) {
-                        const parent = e.target.parentElement;
-                        if (parent) {
-                          const nextParent =
-                            parent.nextElementSibling as HTMLElement;
-                          if (nextParent) {
-                            nextInput =
-                              nextParent.firstElementChild as HTMLInputElement;
-                            // If the first child is not an input, move to the next one
-                            while (nextInput && nextInput.tagName !== "INPUT") {
-                              nextInput =
-                                nextInput.nextElementSibling as HTMLInputElement;
-                            }
-                          }
-                        }
-                      }
-                      if (nextInput) {
-                        nextInput.focus();
-                      }
                     }
-                  }}
-                  onKeyDown={(e) => {
-                    if (
-                      e.key === "Backspace" &&
-                      (e.target as HTMLInputElement).value === ""
-                    ) {
-                      let previousInput = (e.target as HTMLInputElement)
-                        .previousElementSibling as HTMLInputElement;
-                      // If the previous element is not an input, move to the previous one
+                  }
+                }
+                if (nextInput) {
+                  nextInput.focus();
+                }
+              }
+            }}
+            onKeyDown={(e) => {
+              if (
+                e.key === "Backspace" &&
+                (e.target as HTMLInputElement).value === ""
+              ) {
+                let previousInput = (e.target as HTMLInputElement)
+                  .previousElementSibling as HTMLInputElement;
+                // If the previous element is not an input, move to the previous one
+                while (previousInput && previousInput.tagName !== "INPUT") {
+                  previousInput =
+                    previousInput.previousElementSibling as HTMLInputElement;
+                }
+                if (!previousInput) {
+                  const parent = (e.target as HTMLElement).parentElement!;
+                  if (parent) {
+                    const previousParent =
+                      parent.previousElementSibling as HTMLElement;
+                    if (previousParent) {
+                      previousInput =
+                        previousParent.lastElementChild as HTMLInputElement;
+                      // If the last child is not an input, move to the previous one
                       while (
                         previousInput &&
                         previousInput.tagName !== "INPUT"
@@ -65,34 +80,15 @@ export default function MissingWord({ lineArray }: { lineArray: string[] }) {
                         previousInput =
                           previousInput.previousElementSibling as HTMLInputElement;
                       }
-                      if (!previousInput) {
-                        const parent = (e.target as HTMLElement).parentElement!;
-                        if (parent) {
-                          const previousParent =
-                            parent.previousElementSibling as HTMLElement;
-                          if (previousParent) {
-                            previousInput =
-                              previousParent.lastElementChild as HTMLInputElement;
-                            // If the last child is not an input, move to the previous one
-                            while (
-                              previousInput &&
-                              previousInput.tagName !== "INPUT"
-                            ) {
-                              previousInput =
-                                previousInput.previousElementSibling as HTMLInputElement;
-                            }
-                          }
-                        }
-                      }
-                      if (previousInput) {
-                        previousInput.focus();
-                      }
                     }
-                  }}
-                />
-              );
-            })}
-          </div>
+                  }
+                }
+                if (previousInput) {
+                  previousInput.focus();
+                }
+              }
+            }}
+          />
         );
       })}
     </div>
