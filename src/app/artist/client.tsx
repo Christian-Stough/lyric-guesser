5ed23c94dc1ea14ab54855315f89267520643533
Christian Stough <cstough97@gmail.com>
2024-04-19 18:57:23 -0500

    fixed spacing

--- src/app/artist/client.tsx
+++ src/app/artist/client.tsx
@@ -12,25 +12,27 @@ export default function ArtistSelectionClient({
 }) {
   const router = useRouter();
   return (
-    <main className="flex h-screen flex-wrap items-center justify-center gap-8 overflow-y-auto p-4">
-      {artists.map((artist) => (
-        <Button
-          key={artist.id}
-          size="icon"
-          className={`group relative size-48 bg-transparent text-xl text-white hover:bg-transparent `}
-          onClick={() => router.push(`/play/${artist.name}`)}
-        >
-          <Image
-            src={artist.image}
-            fill
-            alt={artist.name}
-            className="absolute left-0 top-0 z-0 opacity-40 transition-opacity duration-300 ease-in-out group-hover:opacity-80"
-          />
-          <span className=" z-10 flex h-fit w-full flex-wrap items-center justify-center text-wrap transition-all duration-300 ease-in-out group-hover:text-white/10 ">
-            {artist.name}
-          </span>
-        </Button>
-      ))}
+    <main className="flex h-screen flex-wrap  items-center justify-center overflow-y-auto p-4">
+      <div className="flex h-fit flex-wrap items-center justify-center gap-8">
+        {artists.map((artist) => (
+          <Button
+            key={artist.id}
+            size="icon"
+            className={`group relative size-48 bg-transparent text-xl text-white hover:bg-transparent `}
+            onClick={() => router.push(`/play/${artist.name}`)}
+          >
+            <Image
+              src={artist.image}
+              fill
+              alt={artist.name}
+              className="absolute left-0 top-0 z-0 opacity-40 transition-opacity duration-300 ease-in-out group-hover:opacity-80"
+            />
+            <span className=" z-10 flex h-fit w-full flex-wrap items-center justify-center text-wrap transition-all duration-300 ease-in-out group-hover:text-white/10 ">
+              {artist.name}
+            </span>
+          </Button>
+        ))}
+      </div>
     </main>
   );
 }
